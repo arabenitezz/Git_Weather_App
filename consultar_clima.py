5add79ca42ea6f0299c12f0a236f3383455dbac3
arabenitezz <abenitezr18@gmail.com>
2024-08-30 17:50:33 -0400

    cambio 7
    queria cambiar de rama y me dijo q haga commit primero

--- consultar_clima.py
+++ consultar_clima.py
@@ -7,7 +7,7 @@ from api_key import API_KEY, API_URL
 
 @click.command()
 @click.option("--lugar", help="Nombre de la ciudad o país de la cual se obtendrá la información sobre el clima")
-@click.option("--formato", default='json', help="Formato en que se van a recibir los datos")
+@click.option("--formato", default='json', help="Formato en que se van a recibir los datos: 'json' para json 'csv' para csv 'texto' para texto")
 @click.option("--unidad", default='metric', help="Unidad de medida: 'metric' para Celsius, 'standard' para Kelvin")
 
 def get_weather(lugar, formato, unidad):
@@ -48,7 +48,7 @@ def get_weather(lugar, formato, unidad):
         click.echo(f'Error al obtener los datos: {err}')
     except Exception as e:
         click.echo(f'Ocurrió un error: {e}')
-        
+
 if __name__ == '__main__':
     get_weather()
 
