c04babe2081144b525728e16a450fd1071cb5f94
arabenitezz <abenitezr18@gmail.com>
2024-08-30 17:40:13 -0400

    cambio 6
    Agregamos para que se puedan enviar las respuestas en formato csv y texto

--- consultar_clima.py
+++ consultar_clima.py
@@ -1,6 +1,9 @@
 import click
 import requests
+import csv
+import io
 from api_key import API_KEY, API_URL
+
 
 @click.command()
 @click.option("--lugar", help="Nombre de la ciudad o país de la cual se obtendrá la información sobre el clima")
@@ -18,27 +21,34 @@ def get_weather(lugar, formato, unidad):
         response = requests.get(API_URL, params=params)
         response.raise_for_status()
 
-        if formato == 'json':
-            weather_data = response.json()
-            place_name = weather_data['name']
-            temp = weather_data['main']['temp']
-            weather_desc = weather_data['weather'][0]['description']
+        weather_data = response.json()
+        place_name = weather_data['name']
+        temp = weather_data['main']['temp']
+        weather_desc = weather_data['weather'][0]['description']
 
-            if unidad == 'metric':
-                unit_symbol = '°C'
-            else:
-                unit_symbol = 'K'
-
-            click.echo(f'El clima en {place_name} es {weather_desc} con una temperatura de {temp}{unit_symbol}.')
+        if unidad == 'metric':
+            unit_symbol = '°C'
         else:
-            # Si es XML o HTML, mostrar el contenido directamente
+            unit_symbol = 'K'
+
+        if formato == 'json':
+            click.echo(f'El clima en {place_name} es {weather_desc} con una temperatura de {temp}{unit_symbol}.')
+        elif formato == 'csv':
+            output = io.StringIO()
+            writer = csv.writer(output)
+            writer.writerow(['Lugar', 'Descripción', 'Temperatura', 'Unidad'])
+            writer.writerow([place_name, weather_desc, temp, unit_symbol])
+            click.echo(output.getvalue())
+        elif formato == 'texto':
+            click.echo(f'Lugar: {place_name}\nDescripción: {weather_desc}\nTemperatura: {temp} {unit_symbol}')
+        else:
             click.echo(response.text)
 
     except requests.exceptions.HTTPError as err:
         click.echo(f'Error al obtener los datos: {err}')
     except Exception as e:
         click.echo(f'Ocurrió un error: {e}')
-
+        
 if __name__ == '__main__':
     get_weather()
 
